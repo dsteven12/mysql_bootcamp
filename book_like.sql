24c555adf6cfa3c55e14da69616b7971822c8543
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-13 23:42:44 +0000

    Understanding difference between underscore vs percentage sign wildcard for searching.

--- book_like.sql
+++ book_like.sql
@@ -1,9 +1,15 @@
-SELECT title, author_fname FROM books WHERE author_fname LIKE '%da%';
+SELECT title AS 'Book Title', 
+CONCAT(author_fname, ' ', author_lname) AS 'Author Name',
+stock_quantity AS 'Quantity Left'
+FROM books
+WHERE stock_quantity LIKE '__';
+
+-- SELECT title, author_fname FROM books WHERE author_fname LIKE '%da%';
  
-SELECT title, author_fname FROM books WHERE author_fname LIKE 'da%';
+-- SELECT title, author_fname FROM books WHERE author_fname LIKE 'da%';
  
-SELECT title FROM books WHERE  title LIKE 'the';
+-- SELECT title FROM books WHERE  title LIKE 'the';
  
-SELECT title FROM books WHERE  title LIKE '%the';
+-- SELECT title FROM books WHERE  title LIKE '%the';
  
-SELECT title FROM books WHERE title LIKE '%the%';
+-- SELECT title FROM books WHERE title LIKE '%the%';
