41e46932eaaccdccb808584e6ed32e8fa9d8ff28
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-13 21:59:21 +0000

    String Functions: CONCAT, SUBSTRING.

--- book_code.sql
+++ book_code.sql
@@ -1,3 +1,9 @@
 SELECT 
     SUBSTRING(REPLACE(title, 'e', '3'), 1, 10)
 FROM books;
+
+SELECT CONCAT(author_fname, ' ', author_lname) AS 'Author Name', 
+CONCAT(SUBSTRING(title, 1, 20), '...') AS 'Shorter Title', 
+released_year AS 'Year Released' 
+FROM books 
+WHERE released_year > 2001;
