0813485a8c01f5c463f7e1f57e66906366c49605
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-13 22:09:43 +0000

    String Functions: Reverse, Char_Length.

--- book_code.sql
+++ book_code.sql
@@ -1,9 +1,15 @@
-SELECT 
-    SUBSTRING(REPLACE(title, 'e', '3'), 1, 10)
-FROM books;
+-- SELECT 
+--     SUBSTRING(REPLACE(title, 'e', '3'), 1, 10)
+-- FROM books;
 
-SELECT CONCAT(author_fname, ' ', author_lname) AS 'Author Name', 
-CONCAT(SUBSTRING(title, 1, 20), '...') AS 'Shorter Title', 
-released_year AS 'Year Released' 
-FROM books 
-WHERE released_year > 2001;
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS 'Author Name', 
+-- CONCAT(SUBSTRING(title, 1, 20), '...') AS 'Shorter Title', 
+-- released_year AS 'Year Released' 
+-- FROM books 
+-- WHERE released_year > 2001;
+
+-- SELECT REVERSE(CONCAT(author_fname, ' ', author_lname)) AS 'Reversed Full Name',
+-- CONCAT(author_fname, ' ', author_lname) AS 'Full Name'
+-- FROM books;
+
+SELECT title AS 'Book Title', CONCAT('The book title is ', CHAR_LENGTH(title), ' characters long.') AS '# of Characters in the Title' from books;
