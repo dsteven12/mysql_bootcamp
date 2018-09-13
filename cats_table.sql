6ac9515cb5d5748fb757ad175c94688e8a5e2a43
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-13 20:39:13 +0000

    first commit

---
+++ cats_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cats
+    (
+        cat_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(100),
+        age INT
+    );
---
+++ testing/insert_cats.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats(name, age)
+VALUES
+('Charlie', 17),
+('Connie', 10),
+('Eli', 22),
+('Peter', 5);
+
+SELECT * FROM cats;
