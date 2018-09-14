2561cfef47b9c6ad777e403c8c7308475f98739e
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-14 02:53:06 +0000

    Added new file to play with count function.

---
+++ agg_functions/count_code.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(title) AS 'Titles with THE' FROM books WHERE title LIKE '%the%';
+-- SELECT COUNT(DISTINCT author_lname, author_fname) FROM books;
+-- SELECT COUNT(DISTINCT author_fname) FROM books;
