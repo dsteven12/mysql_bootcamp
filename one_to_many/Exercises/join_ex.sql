2101b951e0d840be7873554a77454d90201d7917
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-17 09:59:55 -0700

    Restructured one_to_many folder. Finished join tutorial and completed join exercises.

---
+++ one_to_many/Exercises/join_ex.sql
@@ -0,0 +1,43 @@
+-- SELECT first_name, title, grade
+-- FROM students
+-- INNER JOIN papers
+--     ON students.id = papers.student_id
+-- ORDER BY grade DESC;
+
+-- SELECT first_name, title, grade
+-- FROM students
+-- LEFT JOIN papers
+--     ON students.id = papers.student_id;
+    
+-- SELECT first_name, IFNULL(title, 'MISSING'), IFNULL(grade, 0)
+-- FROM students
+-- LEFT JOIN papers
+--     ON students.id = papers.student_id;
+    
+-- SELECT first_name, IFNULL(AVG(grade), 0) AS average
+-- FROM students
+-- LEFT JOIN papers
+--     ON students.id = papers.student_id
+-- GROUP BY first_name
+-- ORDER BY average DESC;
+
+-- SELECT first_name, IFNULL(AVG(grade), 0) AS average
+-- FROM students
+-- LEFT JOIN papers
+--     ON students.id = papers.student_id
+-- GROUP BY students_id
+-- ORDER BY average DESC;
+
+SELECT first_name, 
+IFNULL(AVG(grade), 0) AS average, 
+    CASE 
+        WHEN AVG(grade) IS NULL THEN 'FAILING' 
+        WHEN AVG(grade) >= 75 THEN 'PASSING' 
+        ELSE 'FAILING' 
+    END AS passing_status 
+FROM students 
+LEFT JOIN papers 
+    ON students.id = papers.student_id 
+GROUP BY students.id 
+ORDER BY average DESC;
+    
